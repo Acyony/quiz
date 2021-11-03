919bb5dcf56e174d5e1eb35d52ad39bdf3c490ee
Alcione de França Ribeiro <alcioneribeiro@MacBook-Air-de-Alcione.local>
2021-11-03 18:30:37 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <div className="App">
+                <h1 className="container text-center">
+                    Quiz Game
+                </h1>
+
+            </div>
+        </>
+    );
+}
+
+export default App;
